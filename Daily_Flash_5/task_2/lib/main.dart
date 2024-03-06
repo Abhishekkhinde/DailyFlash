b16a9df3af8315f20dc94212ccaa32c2e03bd7a1
Abhishek Khinde <abhisheknkhinde11@gmail.com>
2024-03-06 22:26:04 +0530

    daily Flash

---
+++ Daily_Flash_5/task_2/lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: DailyFresh(),
+    );
+  }
+}
+
+class DailyFresh extends StatefulWidget {
+  const DailyFresh({super.key});
+
+  @override
+  State createState() => _DailyFreshState();
+}
+
+class _DailyFreshState extends State {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Profile Information"),
+        ),
+        body: Column(
+          children: [
+            Container(
+              height: 100,
+              width: 100,
+              child: Image.network(
+                "https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510_640.jpg",
+                fit: BoxFit.cover,
+              ),
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              child: Image.network(
+                "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
+                fit: BoxFit.cover,
+              ),
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              child: Image.network(
+                "https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg",
+                fit: BoxFit.cover,
+              ),
+            ),
+          ],
+        ));
+  }
+}
