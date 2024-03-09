a3a24f2d084137375a392cf1ccb372ed81d4214d
Abhishek Khinde <abhisheknkhinde11@gmail.com>
2024-03-09 08:55:16 +0530

    daily flash

--- Daily_Flash_6/task_5/lib/main.dart
+++ Daily_Flash_6/task_5/lib/main.dart
@@ -1,8 +1,4 @@
-import 'dart:ffi';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MainApp());
