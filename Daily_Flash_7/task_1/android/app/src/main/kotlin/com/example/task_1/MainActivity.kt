fa34c90a03b066092be41b5c38c4558836a37acd
Abhishek Khinde <abhisheknkhinde11@gmail.com>
2024-03-09 09:18:37 +0530

    daily flash

---
+++ Daily_Flash_7/task_1/android/app/src/main/kotlin/com/example/task_1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.task_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ Daily_Flash_7/task_1/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: DailyFlash(),
+    );
+  }
+}
+
+class DailyFlash extends StatefulWidget {
+  const DailyFlash({super.key});
+  @override
+  State createState() => _DailyFlashState();
+}
+
+class _DailyFlashState extends State {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Daily Flash 7"),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.red,
+            ),
+            Container(
+              height: 80,
+              width: 80,
+              color: Colors.yellow,
+            ),
+            Container(
+              height: 70,
+              width: 80,
+              color: Colors.green,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
